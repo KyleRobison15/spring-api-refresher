e81d9f5bcd1f31b741ede56ce6ea787fb975c97a
KyleRobison15 <kylerobison15@gmail.com>
2025-05-19 13:11:41 -0600

    Implement DTO mapping via MapStruct

--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -1,10 +1,12 @@
 package com.codewithmosh.store.dtos;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Builder
 public class UserDto {
     private Long id;
     private String name;
