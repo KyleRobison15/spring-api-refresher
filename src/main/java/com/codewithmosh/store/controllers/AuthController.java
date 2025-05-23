9ea0387ab9952616737d1a2ead609184c7445273
KyleRobison15 <kylerobison15@gmail.com>
2025-05-23 10:47:57 -0600

    Create an Auth endpoint for retrieving a logged in user

--- src/main/java/com/codewithmosh/store/controllers/AuthController.java
+++ src/main/java/com/codewithmosh/store/controllers/AuthController.java
@@ -2,6 +2,9 @@ package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.JwtResponse;
 import com.codewithmosh.store.dtos.LoginRequest;
+import com.codewithmosh.store.dtos.UserDto;
+import com.codewithmosh.store.mappers.UserMapper;
+import com.codewithmosh.store.repositories.UserRepository;
 import com.codewithmosh.store.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -10,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
@@ -19,6 +23,8 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest request) {
@@ -43,6 +49,25 @@ public class AuthController {
         System.out.println("Validate called");
         var token = authHeader.replace("Bearer ", "");
         return jwtService.validateToken(token);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me(){
+        // 1. Extract the principal from our Security Context
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        // We cast the result of getPrincipal() to a string because
+            // we stored the EMAIL of our users as the principle in the authentication object (in our JWT Auth Filter)
+        var email = (String) authentication.getPrincipal();
+
+        // 2. Find the user in our database
+        var user = userRepository.findByEmail(email).orElse(null);
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        // 3. Map the user in our DB to the UserDto and return it
+        var userDto = userMapper.toDto(user);
+        return ResponseEntity.ok(userDto);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
