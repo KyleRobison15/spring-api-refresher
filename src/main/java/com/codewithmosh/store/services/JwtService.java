0b34ce7aa022afaad8bbce93d84ef753ea30de7b
KyleRobison15 <kylerobison15@gmail.com>
2025-05-23 10:37:04 -0600

    Create JwtAuthenticationFilter for validating requests using JWTs

--- src/main/java/com/codewithmosh/store/services/JwtService.java
+++ src/main/java/com/codewithmosh/store/services/JwtService.java
@@ -1,5 +1,7 @@
 package com.codewithmosh.store.services;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -35,6 +37,27 @@ public class JwtService {
 
                 // Use the information provided above and generate the JWT string
                 .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            return getClaims(token).getExpiration().after(new Date());
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    // We use this method in our JWT Authentication Filter
+    public String getEmailFromToken(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
 }
