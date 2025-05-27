c171f2322e311650cde8bd5152419a021437bb24
KyleRobison15 <kylerobison15@gmail.com>
2025-05-27 15:39:08 -0600

    Create endpoint to get an order by id

--- src/main/java/com/codewithmosh/store/controllers/OrderController.java
+++ src/main/java/com/codewithmosh/store/controllers/OrderController.java
@@ -1,13 +1,16 @@
 package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.OrderDto;
+import com.codewithmosh.store.exceptions.ForbiddenOrderException;
+import com.codewithmosh.store.exceptions.OrderNotFoundException;
 import com.codewithmosh.store.services.OrderService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @AllArgsConstructor
 @RestController
@@ -19,6 +22,21 @@ public class OrderController {
     @GetMapping
     public List<OrderDto> getOrdersForLoggedInUser() {
         return orderService.getAllOrders();
+    }
+
+    @GetMapping("/{orderId}")
+    public OrderDto getOrderById(@PathVariable Long orderId) {
+        return orderService.getOrderById(orderId);
+    }
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<?> handleOrderNotFoundException(OrderNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
+    }
+
+    @ExceptionHandler(ForbiddenOrderException.class)
+    public ResponseEntity<?> handleForbiddenOrderException() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
 
 }
--- src/main/java/com/codewithmosh/store/services/OrderService.java
+++ src/main/java/com/codewithmosh/store/services/OrderService.java
@@ -1,12 +1,15 @@
 package com.codewithmosh.store.services;
 
 import com.codewithmosh.store.dtos.OrderDto;
+import com.codewithmosh.store.exceptions.ForbiddenOrderException;
+import com.codewithmosh.store.exceptions.OrderNotFoundException;
 import com.codewithmosh.store.mappers.OrderMapper;
 import com.codewithmosh.store.repositories.OrderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @AllArgsConstructor
 @Service
@@ -23,4 +26,20 @@ public class OrderService {
                 .map(orderMapper::toDto)
                 .toList();
     }
+
+    public OrderDto getOrderById(Long id) {
+        var user = authService.getCurrentUser();
+        var order = orderRepository.findById(id).orElse(null);
+
+        if (order == null) {
+            throw new OrderNotFoundException();
+        }
+
+        if(!Objects.equals(order.getCustomer().getId(), user.getId())) {
+            throw new ForbiddenOrderException();
+        }
+
+        return orderMapper.toDto(order);
+    }
+
 }
